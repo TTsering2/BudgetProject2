e87d7769ac793ab20f66835495e20a7855e24594
Tsering Tsering <dtseringt2@gmail.com>
2024-04-28 19:04:19 -0400

    commented out tests

--- server/Budgets.DTOs/BudgetReport/ExpenseReportDTO.cs
+++ server/Budgets.DTOs/BudgetReport/ExpenseReportDTO.cs
@@ -1,8 +1,9 @@
+namespace Budgets.DTOs;
 public class ExpenseReportDTO
 {
-    public DateTime StartDate { get; set; };
-    public DateTime EndDate { get; set; };
-    public Dictionary<string, decimal> ExpensesByCategory { get; set; };
-    public IEnumerable<ExpenseDTO> Top3Expenses { get; set; };
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public Dictionary<string, decimal> ExpensesByCategory { get; set; }
+    public IEnumerable<ExpenseDTO> Top3Expenses { get; set; }
 
 }
