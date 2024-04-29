192707c2f8b8e1325b1123d319bccf38fd6c4cf4
wadewilso <uladaharanina@gmail.com>
2024-04-29 12:42:35 -0400

    Add try-catch to controller layer

--- server/Budgets.API/Controllers/UserController.cs
+++ server/Budgets.API/Controllers/UserController.cs
@@ -11,43 +11,71 @@ public class UserController: ControllerBase
         _userService = service;
     }
 
+    //Get All Users
+
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
     public IActionResult ListUsers()
-    {
-        return Ok(_userService.ListUsers());
-    }
-
-    [HttpGet("id/{id}")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    public IActionResult GetUserById(int id)
-    {
-        return Ok(_userService.GetUserById(id));
-    }
-
-    
-    [HttpGet("username/{username}")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    public IActionResult GetUserByUsername(string username)
-    {
-        return Ok(_userService.GetUserByUserName(username));
-    }
-
-    [HttpPost]
-    public IActionResult AddUser(User user){
-        User result = _userService.AddUser(user);
-        if(result != null){
-            UserCreateDTO newUser = new UserCreateDTO(result.Name, result.Username);
-            return Ok(newUser);
-        }else{
-            return BadRequest("Invalid input");
+    {   
+        try{
+            return Ok(_userService.ListUsers());
+        }
+        catch(Exception ex){
+            return BadRequest(ex.Message);
         }
     }
 
+    //Get User by Id
+    [HttpGet("id/{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult GetUserById(int id)
+    {   
+        try{
+            return Ok(_userService.GetUserById(id));
+        }
+        catch(Exception ex){
+            return BadRequest(ex.Message);
+        }
+    }
+
+    
+    //Get User by Username
+    [HttpGet("username/{username}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult GetUserByUsername(string username)
+    {   
+        try{
+            return Ok(_userService.GetUserByUserName(username));
+        }
+        catch(Exception ex){
+            return BadRequest(ex.Message);
+        }
+    }
+
+    //Create a new User
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public IActionResult AddUser(User user){
+
+        try{
+            User result = _userService.AddUser(user);
+            if(result != null){
+                UserCreateDTO newUser = new UserCreateDTO(result.Name, result.Username);
+                return Ok(newUser);
+            }else{
+                return BadRequest("Invalid input");
+            }
+        }
+        catch(Exception ex){
+            return BadRequest(ex.Message);
+        }
+    }
+
+    //Update user Data
     [HttpPut]    
     [ProducesResponseType(StatusCodes.Status202Accepted)]
     public IActionResult UpdateUser(User user){
-        User result = _userService.UpdateUser(user);
+        User? result = _userService.UpdateUser(user);
         if(result!= null){
             UserCreateDTO newUser = new UserCreateDTO(result.Name, result.Username);
             return Accepted(newUser);
@@ -56,31 +84,42 @@ public class UserController: ControllerBase
         }
     }
 
+    //Delete User by Id
     [HttpDelete]
     [ProducesResponseType(StatusCodes.Status200OK)]
     public IActionResult DeleteUser(int userId){
-        bool result = _userService.DeleteUser(userId);
-        if(result){
-            return Accepted(result);
+        try{
+            bool result = _userService.DeleteUser(userId);
+            if(result){
+                return Accepted(result);
+            }
+            else{
+                return BadRequest("Invalid input");
+            }
         }
-        else{
-            return BadRequest("Invalid input");
-
+        catch(Exception ex){
+            return BadRequest(ex.Message);
         }
+        
     }
 
-    /*Verification*/
+    /*Verify user has entry in DB*/
     [HttpPost("login")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     public IActionResult Login(string username, string password){
-    
-        bool AuthUser = _userService.ValidateUserStatus(username,password);
-        if(AuthUser){
-            User user = _userService.GetUserByUserName(username);
-            return Ok(new { UserId = user.Id });
+        try{
+            bool AuthUser = _userService.ValidateUserStatus(username,password);
+            if(AuthUser){
+                User user = _userService.GetUserByUserName(username);
+                return Ok(new { UserId = user.Id });
+            }
+            else{
+                return Unauthorized();
+            }
         }
-        else{
-            return Unauthorized();
+        catch(Exception ex){
+            return BadRequest(ex.Message);
         }
+
     }
 }
