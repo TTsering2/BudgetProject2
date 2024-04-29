2a2b36d91384379abaffc1d758b778848b02ecf4
brianV05 <brianve2001@gmail.com>
2024-04-29 16:19:35 -0400

    fixed name method error in service class

--- server/Budgets.API/Services/IncomeService.cs
+++ server/Budgets.API/Services/IncomeService.cs
@@ -6,34 +6,34 @@ namespace Budgets.Services;
 
 public class IncomeService : IIncomeService{
 
-    private readonly IIncomeRepository _repo;
-    public IncomeService (IIncomeRepository repo){
-        _repo = repo;
+    private readonly IIncomeRepository _incomerepo;
+    public IncomeService (IIncomeRepository incomerepo){
+        _incomerepo = incomerepo;
     }
 
 
 
     public async Task<IEnumerable<IncomeDTO>>? GetIncomeByUserIdAsync(int userId){
-        return await _repo.GetIncomeByUserIdAsync(userId);
+        return await _incomerepo.GetIncomeByUserIdAsync(userId);
     }
     public async Task<IncomeDTO>? GetIncomeByUserIdAndIncomeIdAsync(int userId, int incomeId){
-        return await _repo.GetIncomeByUserIdAndIncomeIdAsync(userId, incomeId);
+        return await _incomerepo.GetIncomeByUserIdAndIncomeIdAsync(userId, incomeId);
     }
     public async Task<IEnumerable<IncomeDTO>>? GetIncomeByUserIdAndIncomeTypeAsync(int userId, string incomeType){
-        return await _repo.GetIncomeByUserIdAndIncomeTypeAsync(userId,incomeType);
+        return await _incomerepo.GetIncomeByUserIdAndIncomeTypeAsync(userId,incomeType);
     }
     public async Task AddAnIncomeAsync(IncomeCreateDTO entity){
-        await _repo.AddAnIncomeAsync(entity);
+        await _incomerepo.AddAnIncomeAsync(entity);
     }
     public async Task DeleteAnIncomeAsync(int incomeId){
-        await _repo.DeleteAnIncomeAsync(incomeId);
+        await _incomerepo.DeleteAnIncomeAsync(incomeId);
     }
 
     public async Task UpdateAnIncomeAsync(int incomeId, IncomeUpdateDTO entity){
-        await _repo.UpdatAnIncomeeAsync(incomeId, entity);
+        await _incomerepo.UpdatAnIncomeeAsync(incomeId, entity);
     }
     public async Task<IEnumerable<IncomeDTO>>? GetIncomeByUserIdAndDateRangeAsync(int userId, DateTime startDate, DateTime endDate){
-        return await _repo.GetIncomeByUserIdAndDateRangeAsync(userId, startDate,endDate);
+        return await _incomerepo.GetIncomeByUserIdAndDateRangeAsync(userId, startDate,endDate);
     }
 }
 
