533e5a71d759966baa10fefb1de2d2d2a7d08cd4
uladaharanina <uladaharanina@gmail.com>
2024-05-02 08:55:11 -0700

    Update namespaces for DTOs

--- server/Budgets.Data/Repositories/Interfaces/IUserRepository.cs
+++ server/Budgets.Data/Repositories/Interfaces/IUserRepository.cs
@@ -1,13 +1,16 @@
 
 using Budgets.DTOs;
 using Budgets.Models;
+using System.Threading.Tasks;
+
+namespace Budgets.Data;
 
 public interface IUserRepository
 {   
-    public IEnumerable<User> ListUsers();
-    public User GetUserById(int id);
-    public User GetUserByUsername(string username);
-    public User AddUser(User user);
-    public User UpdateUser(User user);
-    public User DeleteUser(User user);
+    public Task<IEnumerable<User>>? ListUsers();
+    public Task<User>? GetUserById(int id);
+    public Task<User>? GetUserByUsername(string username);
+    public Task<User>? AddUser(User user);
+    public Task<User>? UpdateUser(User user);
+    public Task<User>? DeleteUser(User user);
 }
