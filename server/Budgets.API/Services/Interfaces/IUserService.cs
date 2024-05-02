7fac7d1e3c15f45ebcf0cd969599c61f5a22e6ba
uladaharanina <uladaharanina@gmail.com>
2024-05-02 08:48:01 -0700

    Fix DTO, add async

--- server/Budgets.API/Services/Interfaces/IUserService.cs
+++ server/Budgets.API/Services/Interfaces/IUserService.cs
@@ -6,13 +6,13 @@ namespace Budgets.Services;
 public interface IUserService
 {
 
-    public List<User> ListUsers();
-    public User AddUser(User user);
-    public User UpdateUser(User user);
-    public bool DeleteUser(int id);
-    public User GetUserById(int id);
-    public User GetUserByUserName(string username);
+    public Task<IEnumerable<User>>? ListUsers();
+    public Task<User>? AddUser(UserCreateDTO user);
+    public Task<User>? UpdateUser(UserUpdateDTO user);
+    public Task<bool> DeleteUser(int id);
+    public Task<User>? GetUserById(int id);
+    public Task<User>? GetUserByUserName(string username);
     
     //Implement validation of login status
-    public bool ValidateUserStatus(string username, string password);
+    public Task<bool> ValidateUserStatus(string username, string password);
 }
