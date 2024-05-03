052aa108b346405c1375e24a4b84a24aea77929e
uladaharanina <uladaharanina@gmail.com>
2024-05-03 09:51:47 -0700

    update style of landing page

--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -1,7 +1,10 @@
 
 const Header = () => {
+
+    //or userState or Context
     return (
         <header className="header text-white p-6">
+        
             <section className="flex flex-row  items-center justify-between pb-4 w-10/12 mx-auto">
                 <div className="flex flex-row mr-6 items-center	">
                     <img src="./assets/SpendWiseTop.svg" alt="logo" className="mr-4 w-40"></img>
