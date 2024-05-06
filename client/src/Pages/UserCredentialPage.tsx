e9a2b06d9c937f6f4710bb63fc64a997b012ad0e
Tsering Tsering <dtseringt2@gmail.com>
2024-05-06 17:39:46 -0400

    routes are created and context working fully as intended

---
+++ client/src/Pages/UserCredentialPage.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react"
+
+const UserCredentialPage: FC = () => {
+
+    return (
+        <div>
+            UserCredentialPage
+        </div>
+    )
+}
+
+export default UserCredentialPage
